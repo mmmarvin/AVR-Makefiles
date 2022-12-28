82597c389381bb5e2ed58c06bbb985c71bbb8d47
Marvin Manese <43771610+mmmarvin@users.noreply.github.com>
2022-12-28 01:25:20 +0000

    fixed upload script
    fixed upload script to check for existence of input parameter

--- upload.sh
+++ upload.sh
@@ -1,1 +1,17 @@
-avrdude -F -V -c arduino -p ATMEGA328P -P /dev/TTYACM0 -b 115200 -U flash:w:$1
+function print_usage()
+{
+  echo Usage: $0 [HEX Filename]
+}
+
+if [ $# -eq 1]
+  then
+    if [[ $1 == "--help" ]]
+        then
+          print_usage
+    else
+      avrdude -F -V -c arduino -p ATMEGA328P -P /dev/TTYACM0 -b 115200 -U flash:w:$1
+    fi
+else
+  echo Invalid usage!
+  print_usage
+fi
